bd73256321c8a35d20f92191e0fa5e89709217d8
tanujyadav70 <tanujyadavjhajjar@gmail.com>
2026-03-12 11:00:07 +0530

    Fix NotFound import for Render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import NotFound from "@/pages/not-found";
+import NotFound from "@/pages/not-found.tsx";
 import { Toaster } from "@/components/ui/toaster.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip.tsx";
 
